68855cbf72e0c428963a683388a3d43791f11548
Alexey Illarionov <littlesavage@rambler.ru>
2013-07-15 17:04:29 +0400

    Окружение stage при развертывании

--- config/capdeploy.rb
+++ config/capdeploy.rb
@@ -17,7 +17,7 @@ server 'icfdev.ru', :app, :web, :db, :primary => true
 set(:current_branch) { `git branch`.match(/\* (\S+)\s/m)[1] || raise("Couldn't determine current branch") }
 set :branch, defer { current_branch } unless exists?(:branch)
 
-set :rails_env, "development"
+set :rails_env, "stage"
 
 set :keep_releases, 3
 
